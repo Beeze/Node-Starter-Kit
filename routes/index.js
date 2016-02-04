82312030c3e6ddebbe8e08b878cfa87738faddcf
beezebrown <m.brown298@gmail.com>
2016-02-03 21:34:41 -0500

    Added omari and sedrick's domain

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'My webpage', name: 'marc' });
 });
 
+router.get('/sedcash', function(req, res, next) {
+  res.render('sedcash', { title: 'My webpage', name: 'Sedrick' });
+});
+
+router.get('/omari', function(req, res, next) {
+  res.render('omari', { title: 'My webpage', name: 'Omari' });
+});
 module.exports = router;
